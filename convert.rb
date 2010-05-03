b1c54e2f20f847a4272bd0e8017d695144cf7806
Scott Cytacki <scott@concord.org>
2010-05-03 10:29:36 -0400

    Refactored to create object model, added min event size
    Apples ical visually overlays events if they don't overlap in time.  This fix extends each event to be at least 8mins
    that way events close to each other will overlap in time which causes them to be layed out next to each other instead
    of ontop of each other.
    To achived this there is now a data model, which represent the visit trails, and then it is turned into ical events.

--- convert.rb
+++ convert.rb
@@ -20,20 +20,88 @@ query = "select * from moz_historyvisits, moz_places " +
         "and moz_historyvisits.visit_date<#{end_time}"
 length = 0
 cal = Icalendar::Calendar.new
-db.execute(query) {|row|
-  event_start = Time.at(row[3].to_i / 1000000)
 
-  cal.event {
-    puts event_start
-    dtstart event_start.to_datetime
-    dtend (event_start + 60).to_datetime
-    summary row[8]
-    url row[7]
-  }
-  length += 1
+class VisitEvent
+  attr_accessor :visit_date, :from_visit, :url, :title, :session
+  
+  def initialize(row)
+    @from_visit = row[1].to_i
+    @visit_date = row[3].to_i
+    @session = row[5].to_i
+    @url = row[7]
+    @title = row[8]
+  end
+  
+  def visit_time
+    Time.at(self.visit_date / 1000000)
+  end
+  
+end
+
+class VisitEventTrail
+  attr_accessor :events
+  
+  def initialize()
+    @events = []
+  end  
+  
+  def add(event)
+    events.push event
+  end
+  
+  def dtstart
+    events.first.visit_time.to_datetime
+  end
+  
+  def dtend
+    first_time = events.first.visit_time
+    # use a 8 minute minimun event size
+    end_time = first_time + 8*60
+    if events.last.visit_time > end_time
+      end_time = events.last.visit_time
+    end
+    end_time.to_datetime
+  end
+  
+  def description
+    events.collect{|event|
+      event.visit_time.strftime("%I:%M:%S") + ": " + event.title
+    }.join("\n")    
+  end
+end
+
+visit_events = []
+visit_trails = []
+
+db.execute(query) {|row|
+  last_event = visit_events.last
+  event = VisitEvent.new(row)
+  visit_events.push(event)
+
+  puts event.visit_time
+    
+  # look for overlaping events
+  if (last_event and (last_event.visit_time + 180) > event.visit_time)
+    visit_trails.last.add event
+    next
+  end
+
+  visit_trail = VisitEventTrail.new
+  visit_trail.add event
+  visit_trails.push visit_trail
 }
 
-puts "length: #{length}"
+visit_trails.each {|trail|
+  cal.event {
+    dtstart trail.dtstart
+    dtend trail.dtend
+    description trail.description
+    summary trail.events.first.title
+    url trail.events.first.url    
+  }  
+}
+
+puts "length: #{visit_trails.length}"
 File.open("history.ics", 'w') {|f|
   f.write cal.to_ical
 }
