eed7598ee6e5e4a921cd7bd4f72b72c961d655fa
Scott Cytacki <scott@concord.org>
2011-02-02 12:27:23 -0500

    Monkey patch to make ical writing more efficient

--- ical-from-sent-email.rb
+++ ical-from-sent-email.rb
@@ -56,6 +56,43 @@ msgs.each_with_index{|id, idx|
   }    
 }
 
+# Icalendar::Component
+# def to_ical
+#       print_component do
+#         s = ""
+#         @components.each_value do |comps|
+#           comps.each { |component| s << component.to_ical }
+#         end
+#         s
+#       end
+#     end
+module Icalendar
+  class Component
+    def print_component_file(f)
+      # Begin a new component
+      f << "BEGIN:#{@name.upcase}\r\n"
+
+      # Then the properties
+      f <<  print_properties
+
+      # sub components
+      yield
+
+      # End of this component
+      f << "END:#{@name.upcase}\r\n"
+    end
+    
+    def to_ical_file(f)
+      print_component_file(f) do
+         @components.each_value do |comps|
+           comps.each { |component| component.to_ical_file(f) }
+         end
+      end
+    end
+  end
+end
+
+
 File.open(ics_file, 'w') {|f|
-  f.write cal.to_ical
+  cal.to_ical_file(f)
 }
