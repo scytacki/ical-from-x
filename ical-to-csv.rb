49f96214bf5c4747e8b347ea41de97e2caef34f0
Scott Cytacki <scytacki@concord.org>
2012-04-30 11:14:28 -0400

    add headers to columns

--- ical-to-csv.rb
+++ ical-to-csv.rb
@@ -14,9 +14,11 @@ cal = cals.first
 
 #Write a CSV File
 CSV.open("#{dir}/tasks.csv", 'w') do |csv|
+  csv << ['start', 'end', 'hours', 'task', 'type', 'project']
   cal.events.each{|event|
     # the returned number is a rational and is the number of days difference
     hours = ((event.end-event.start)*24).to_f
     csv << [event.start.to_s, event.end.to_s, hours.to_s, event.summary]
   }
 end
+puts "Wrote: #{dir}/tasks.csv"
