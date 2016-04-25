35e8601d2e03b5485131d49720330e0e025a65df
Simon Kelly <skelly@dimagi.com>
2016-04-25 15:05:09 +0200

    make it easier for others to use

--- ical-from-git-history.rb
+++ ical-from-git-history.rb
@@ -7,37 +7,14 @@ require 'rubygems'
 require 'trollop'
 require 'icalendar'
 
-devroot = '/Users/scytacki/Development/'
-author = 'scytacki'
-
-repositories = Dir.glob('/Users/scytacki/Development/*/.git').map{|dir| File.dirname dir}
-
-# repositories = [
-#   ['XPP', 'rigse'],
-#   ['XPP', 'rails-ref-tree'],
-#   ['XPP', 'rigse-rails3'],
-#   ['XPP', 'rigse-1.9.3'],
-#   ['XPP', 'sensors-hobo'],
-#   ['ITSISU', 'rigse-itsisu'],
-#   ['ITSISU', 'video-paper-builder'],
-#   ['CLEAR', 'mysystem_sc'],
-#   ['CLEAR', 'wise4-vagrant'],
-#   ['CLEAR', 'MySystem-Wise-Integration-WIP'],
-#   ['SG', 'hobo'],
-#   ['SG', 'hobo-matrix'],
-#   ['SG', 'hobo-polymorphic-list'],
-#   ['SG', 'Smartgraphs'],
-#   ['SG', 'smartgraphs-authoring'],
-#   ['SG', 'smartgraphs-generator'],
-#   ['GG', 'genigames-lc']
-# ]
-
 # commandline option parsing, trollop is much more concise than optparse
 options = Trollop::options do
   banner "Usage: #{$0} [options]"
   opt :month, 'Month to filter results with', :type => :int
   opt :year, 'Year to filter results with', :type => :int
   opt :out, 'Output file to put the resulting ics', :type => :string
+  opt :author, 'Git author to search for', :type => :string
+  opt :repositories, 'Paths to repositories to search', :type => :strings
 end
 
 date = Date.civil(options[:year], options[:month])
@@ -97,10 +74,10 @@ end
 
 commits = {}
 
-repositories.each do |path|
+options[:repositories].each do |path|
   Dir.chdir(path) do
     github_site = Commit.find_github_site `git remote -v`
-    commit_subjects = `git log HEAD --no-merges --reverse --since='#{month_start}' --pretty=format:"%H\t%ad\t%cd\t%s%n" --author=#{author}`
+    commit_subjects = `git log HEAD --no-merges --reverse --since='#{month_start}' --pretty=format:"%H\t%ad\t%cd\t%s%n" --author=#{options[:author]}`
     commit_lines = commit_subjects.split(/\n+/)
     commit_lines.each{|commit_line|
       current_commit = Commit.parse(commit_line)
@@ -120,15 +97,15 @@ cal = Icalendar::Calendar.new
 duration = 10*60
 
 commits.values.each do |commit|
-  cal.event{
-    dtstart (commit.author_date.to_time - duration).to_datetime
-    dtend commit.author_date
-    summary "#{commit.summary}"
-    description "#{commit.summary} #{commit.repositories_str} #{commit.sha}"
+  cal.event do |e|
+    e.dtstart = (commit.author_date.to_time - duration).to_datetime
+    e.dtend = commit.author_date
+    e.summary = "#{commit.summary}"
+    description = "#{commit.summary} #{commit.repositories_str} #{commit.sha}"
     if commit.github_url
-      url commit.github_url
+      e.url = commit.github_url
     end
-  }
+  end
 end
 
 File.open(options[:out], 'w') {|f|
